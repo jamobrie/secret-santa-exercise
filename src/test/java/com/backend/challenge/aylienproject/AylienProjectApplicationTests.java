312dc031e192eb7854bbd47b40b6dae686e09057
James O'Brien <jamesobrien442@gmail.com>
2021-12-02 17:59:06 +0000

    Commit 1 - Creating Base Project To Work Off Of

---
+++ src/test/java/com/backend/challenge/aylienproject/AylienProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.challenge.aylienproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AylienProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
